1cef5bafebd36faa883c464a1344364080a328bc
Bryan <kenshin2004528@gmail.com>
2018-05-19 13:51:42 +0800

    fixed path of python

--- bootstrap/spark.sh
+++ bootstrap/spark.sh
@@ -1,5 +1,6 @@
 
-export PYSPARK_DRIVER_PYTHON=/usr/bin/ipython
-export PYSPARK_PYTHON=/usr/bin/ipython
+export PYSPARK_DRIVER_PYTHON=/root/miniconda3/bin/ipython
+export PYSPARK_PYTHON=/root/miniconda3/bin/ipython
 export SPARK_HOME=/workspace/spark
 export PATH=$SPARK_HOME/bin:$PATH
+export PATH=${HOME}/miniconda3/bin:${PATH}
